df1c183ba39feecb8e92476aeacc3d4f7bc0fbc6
Brad Anderson <brad@sankatygroup.com>
2011-12-21 09:55:49 -0500

    rework cluster setup with a config file in priv/

--- src/merle_cluster.erl
+++ src/merle_cluster.erl
@@ -1,31 +1,69 @@
 -module(merle_cluster).
 
--export([configure/2]).
+-export([configure/0, configure/1, configure/2]).
 
-index_map(F, List) ->
-    {Map, _} = lists:mapfoldl(fun(X, Iter) -> {F(X, Iter), Iter +1} end, 1, List),
-    Map.
+configure() ->
+  {ok, Conf} = get_conf(),
+  MemcachedHosts = proplists:get_value(memcached_hosts, Conf),
+  ConnectionsPerHost = proplists:get_value(connections_per_host, Conf),
+  configure(MemcachedHosts, ConnectionsPerHost).
+
+configure(ConnectionsPerHost) ->
+  {ok, Conf} = get_conf(),
+  MemcachedHosts = proplists:get_value(memcached_hosts, Conf),
+  configure(MemcachedHosts, ConnectionsPerHost).
 
 configure(MemcachedHosts, ConnectionsPerHost) ->
-    SortedMemcachedHosts = lists:sort(MemcachedHosts),
-    DynModuleBegin = "-module(merle_cluster_dynamic).
+  clear_existing_connections(),
+  SortedMemcachedHosts = lists:sort(MemcachedHosts),
+  DynModuleBegin = "-module(merle_cluster_dynamic).
          -export([get_server/1]).
-         get_server(ClusterKey) -> N = erlang:phash2(ClusterKey, ~p), 
+         get_server(ClusterKey) -> N = erlang:phash2(ClusterKey, ~p),
                                    do_get_server(N).
                                    ",
-     DynModuleMap = "do_get_server(~p) -> {\"~s\", ~p}; ",
-     DynModuleEnd = "do_get_server(_N) -> throw({invalid_server_slot, _N}).\n",
-     ModuleString = lists:flatten([
-                   io_lib:format(DynModuleBegin, [length(SortedMemcachedHosts)]),
-                  index_map(fun([Host, Port], I) -> 
-				io_lib:format(DynModuleMap, [I-1, Host, Port]) 
-			end, SortedMemcachedHosts),
-                  DynModuleEnd
-                  ]),
-   {M, B} = dynamic_compile:from_string(ModuleString),
-   code:load_binary(M, "", B),
-   lists:foreach(fun([Host, Port]) ->
-        lists:foreach(fun(_) -> 
-		supervisor:start_child(merle_sup, [[Host, Port]]) 
-	end, lists:seq(1, ConnectionsPerHost))
-   end, SortedMemcachedHosts).
+  DynModuleMap = "do_get_server(~p) -> {\"~s\", ~p}; ",
+  DynModuleEnd = "do_get_server(_N) -> throw({invalid_server_slot, _N}).\n",
+  ModuleString = lists:flatten([
+    io_lib:format(DynModuleBegin, [length(SortedMemcachedHosts)]),
+    index_map(fun([Host, Port], I) ->
+                  io_lib:format(DynModuleMap, [I-1, Host, Port])
+              end, SortedMemcachedHosts),
+    DynModuleEnd
+  ]),
+  {M, B} = dynamic_compile:from_string(ModuleString),
+  code:load_binary(M, "", B),
+  lists:foreach(fun([Host, Port]) ->
+                    load_workers(ConnectionsPerHost, Host, Port)
+                end, SortedMemcachedHosts).
+
+%%
+%% internal
+%%
+
+index_map(F, List) ->
+  {Map, _} = lists:mapfoldl(fun(X, Iter) ->
+    {F(X, Iter), Iter +1}
+  end, 1, List),
+  Map.
+
+load_workers(ConnectionsPerHost, Host, Port) ->
+  lists:foreach(fun(_) ->
+    supervisor:start_child(merle_sup, [[Host, Port]])
+  end, lists:seq(1, ConnectionsPerHost)).
+
+clear_existing_connections() ->
+  Children = supervisor:which_children(merle_sup),
+  close(Children).
+
+close([]) -> ok;
+close([{_, ChildPid, _, _}|Rest]) ->
+  supervisor:terminate_child(merle_sup, ChildPid),
+  supervisor:delete_child(merle_sup, ChildPid),
+  close(Rest).
+
+get_conf() ->
+  Filename = code:priv_dir(merle) ++ "/merle.conf",
+  case file:consult(Filename) of
+    {ok, Terms} -> {ok, Terms};
+    {error, enoent} -> throw({error, no_config_file})
+  end.
--- src/merle_sup.erl
+++ src/merle_sup.erl
@@ -1,20 +1,17 @@
 -module(merle_sup).
 
--export([start_link/0, start_link/2, init/1]).
+-export([start_link/0, init/1]).
 
 -export([start_child/1]).
 
 -behaviour(supervisor).
 
 start_link() ->
-  start_link([["localhost",22133]], 5).
-
-start_link(Instances, ConnectionsPerInstance) ->
     Pid = case supervisor:start_link({local, ?MODULE}, ?MODULE, []) of
             {ok, P} -> P;
             {error, {already_started, P}} -> P
           end,
-    merle_cluster:configure(Instances, ConnectionsPerInstance),
+    merle_cluster:configure(),
     {ok, Pid}.
 
 start_child(N) ->
